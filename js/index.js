6f8d5852eddf3a705b9d65589778cdbe4fadac61
Juancamilobs <juancamilobs92@gmail.com>
2018-11-05 16:37:16 -0500

    Login function
    Login functions have been created with the corresponding php files. PHP connector has been created.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
+
+  $('#login').on( 'click', function(){
+    var fname = $('#user_name').val();
+    var fpass = $('#password').val();
+    login(fname,fpass);
+  })
+
 });
+
+function login(username,password){
+
+  $.ajax({
+    url : "php/login.php",
+    method : "POST",
+    data : {
+      fname: username,
+      fpass: password
+    },
+    success: function (php_response){
+      if (php_response == '"OK"'){
+        alert('ingreso existoso');
+        $('#login-form').trigger("reset");
+      }else{
+        alert ("El usuario o contraseña ingresada no son correctos, intenta de nuevo.");
+      }
+    }
+  });
+}
---
+++ php/login.php
@@ -0,0 +1,5 @@
+<?php
+  require 'C:\xampp\htdocs\Consulti\Consulti---back\php\login.php'
+
+
+ ?>
