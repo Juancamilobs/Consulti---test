125f4bf04e54e7252ae281df938b842476aeb1c7
Juancamilobs <juancamilobs92@gmail.com>
2018-11-08 00:07:07 -0500

    appointment form created
    The front end of the appointment form was created

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
-
+//------------Login page-----------//
   $('#login').on( 'click', function(){
     var fname = $('#user_name').val();
     var fpass = $('#password').val();
     login(fname,fpass);
   });
-
   $(document).keypress( function (e) {
     var key = e.which;
     if (key == 13) {
@@ -16,15 +15,33 @@ $(document).ready(function(){
       return false;
     }
   })
-  $('#welcome-text').text(function () {
-    return $(this).text().replace("###user###", sessionStorage.username);
+//------------Home Page-----------//
+  $('#user-identification').text(function () {
+    return $(this).text().replace("###username###", sessionStorage.username);
   });
-
-
+  $('#logOut').on( 'click' , function () {
+    if (confirm ("¿Deseas salir del sistema?")){
+      logOut();
+    }
+  });
+  $('#logOut-mobile').on( 'click' , function () {
+    if (confirm ("¿Deseas salir del sistema?")){
+      logOut();
+    }
+  })
+  $('.datepicker').datepicker();
+  $('.timepicker').timepicker({
+        default: 'now',
+        twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
+        donetext: 'OK',
+        format: "HH:ii:SS",
+        autoclose: false,
+        vibrate: true
+    });
 });
-
+//-----------------Funciones-----------------//
+//-------Iniciar sesión--------//
 function login(username,password){
-
   $.ajax({
     url : "php/login.php",
     method : "POST",
@@ -33,9 +50,8 @@ function login(username,password){
       fpass: password
     },
     success: function (php_response){
-
       answer = JSON.parse(php_response);
-      if (answer !== null){
+      if (answer != null){
         if(answer.acceso == "OK"){
           window.sessionStorage.username = answer.name;
           resetForm();
@@ -44,24 +60,22 @@ function login(username,password){
           alert ("Error de credenciales");
           resetForm();
         }
+
       }else {
         alert ("Error de credenciales");
         resetForm();
-
-      $('#login-form').trigger("reset");
-      if (php_response == '"OK"'){
-        alert('ingreso existoso');
-
-      }else{
-        alert ("El usuario o contraseña ingresada no son correctos, intenta de nuevo.");
-
       }
-
     }
-  });
-}
-
+  })
+};
+//-------Reiniciar formulario--------//
 function resetForm(){
   $('#user_name').val("");
   $('#password').val("");
+}
+//-------Cerrar sesión--------//
+function logOut(){
+  document.location.href = "../";
+  sessionStorage.clear();
+
 }
